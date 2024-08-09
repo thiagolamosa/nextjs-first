d4d38ed573f45bbd114de5b1cc20ffbeb6348419
Thiago Lamosa <thiagolamosa@hotmail.com>
2024-08-08 22:39:12 -0300

    first page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function home() {
+    return (
+        <div>
+            <h1>Hi, there!</h1>
+            <h2>Learning lessons</h2>
+            <p>Text...</p>
+        </div>
+    )
+}
+
+export default home;
