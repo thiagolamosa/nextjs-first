f5ad63f5c3457b6b05ea901087d39cdf9d5302d5
Thiago Lamosa <thiagolamosa@hotmail.com>
2024-08-09 09:41:43 -0300

    bootstrap test

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,73 @@
 import { useState } from "react";
+import Header from "../includes/header";
+import Footer from "../includes/footer";
 
-function home() {
+export default function Home() {
+
     return (
-        <div>
-            <h1>Hi, there!</h1>
-            <h2>Learning lessons</h2>
-            <p>Text...</p>
+        <div class="container-fluid">
+            <Header />
+            <Body />
+            <Footer />
         </div>
-    )
+    );
 }
 
-export default home;
+
+function Body() {
+    return (
+        <section>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="jumbotron">
+                            <h2>
+                                Hello, world!
+                            </h2>
+                            <p>
+                                This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary btn-large" href="#">Learn more</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <h2>
+                            Heading
+                        </h2>
+                        <p>
+                            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                        </p>
+                        <p>
+                            <a class="btn" href="#">View details »</a>
+                        </p>
+                    </div>
+                    <div class="col-md-4">
+                        <h2>
+                            Heading
+                        </h2>
+                        <p>
+                            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                        </p>
+                        <p>
+                            <a class="btn" href="#">View details »</a>
+                        </p>
+                    </div>
+                    <div class="col-md-4">
+                        <h2>
+                            Heading
+                        </h2>
+                        <p>
+                            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                        </p>
+                        <p>
+                            <a class="btn" href="#">View details »</a>
+                        </p>
+                    </div>
+                </div>
+                
+            </section>
+    );
+}
